f3ead76fa45f98dff739668e7c25dc18dd51192e
Virginia Dooley <vcdooley@gmail.com>
2019-02-28 13:35:29 +0000

    Done.

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -6,8 +6,8 @@ describe 'Patient' do
     @patient = Patient.create(name: "Russell Wilson", age: 27)
     @mcdreamy = Doctor.create(name: "Derek Shepherd", department: "Neurosurgery")
     @meredith = Doctor.create(name: "Meredith Grey", department: "Internal Medicine")
-    @appointment1 = Appointment.create(appointment_datetime: DateTime.new(2016, 10, 1), doctor: @mcdreamy, patient: @patient  )
-    @appointment2 = Appointment.create(appointment_datetime: DateTime.new(2016, 10, 1), doctor: @meredith, patient: @patient  )
+    @appointment1 = Appointment.create(appointment_datetime: DateTime.new(2016, 10, 1), doctor_id: @mcdreamy, patient_id: @patient  )
+    @appointment2 = Appointment.create(appointment_datetime: DateTime.new(2016, 10, 1), doctor_id: @meredith, patient_id: @patient  )
   end
 
   it 'has a name' do
