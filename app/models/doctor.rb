2d21ce38d82ef0209a604e65170f0d56971115c1
Virginia Dooley <vcdooley@gmail.com>
2019-02-28 17:55:00 +0000

    Done.

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,4 @@
 class Doctor < ActiveRecord::Base
   has_many :appointments
-  has_many :patients, :through => :appointments, :source => :patients
+  has_many :patients, through: :appointments, source: :patient
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,4 @@
 class Patient < ActiveRecord::Base
   has_many :appointments
-  has_many :doctors, :through => :appointments, :source => :doctors
+  has_many :doctors, through: :appointments, source: :doctor
 end
